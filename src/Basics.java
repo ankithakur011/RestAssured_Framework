8726ca1e69ef2f1737ce240cc283cce6c30372ef
PFAD\AThakur <athakur@premierfarnell.com>
2023-01-26 14:45:12 +0000

    Added basic REST tests

---
+++ src/Basics.java
@@ -0,0 +1,63 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import resources.payload;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.Assert;
+
+import static io.restassured.RestAssured.*;
+
+public class Basics {
+	
+	public static void main(String[] args) {
+		
+		//Given - used to declare all the Pre-requisite like query params, authorization, header, body
+		//When - used to pass the HTTP request type (PUT,POST,PUT,DELETE) along with resource as a parameter
+		//Then - used to validate the response and extract and key value for assertion if necessary
+		
+		
+		//POST Request and storing key value from the response code 
+		
+		RestAssured.baseURI="http://rahulshettyacademy.com/";
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body(payload.body())
+		.when().post("maps/api/place/add/json")
+		.then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+		.header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
+		
+		System.out.println("Response is: "+response);
+		
+		JsonPath js = new JsonPath(response);
+		String place_id = js.get("place_id");
+		System.out.println("Place ID value is: "+place_id);
+		
+		
+		//PUT Request - Update Address value
+		
+		String newAddress = "70 Summer walk, Greenwich";
+		given().log().all().queryParam("place_id", place_id).queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body("{\r\n"
+				+ "\"place_id\":\""+ place_id +"\",\r\n"
+				+ "\"address\":\""+ newAddress +"\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}")
+		.when().put("maps/api/place/update/json")
+		.then().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+		
+		
+		//GET Request - To get the updated value of Address
+		
+		String get_Response = given().log().all().queryParam("place_id", place_id).queryParam("key", "qaclick123")
+		.when().get("maps/api/place/get/json")
+		.then().log().all().statusCode(200).extract().response().body().asString();
+		
+		JsonPath js1 = new JsonPath(get_Response);
+		String actualAddress = js1.get("address");
+		System.out.println("Value of actual address from the GET request is: "+actualAddress);
+		
+		Assert.assertEquals(actualAddress, newAddress);
+		
+		
+	}
+
+}
